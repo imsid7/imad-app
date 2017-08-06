48fdb0272b48bccfaac3f82a34057e8c9adf5d42
imsid7 <30745244+imsid7@users.noreply.github.com>
2017-08-06 12:11:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function (req, res) {
+    res.send("article one rquested");
+});
+
+app.get('/article-two',function (req, res) {
+    res.send("article two rquested");
+});
+
+app.get('/article-three',function (req, res) {
+    res.send("article three rquested");
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
