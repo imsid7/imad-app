bc250bbc5918bc69e5e340d7fc9c239cea6cd906
imsid7 <30745244+imsid7@users.noreply.github.com>
2017-08-06 13:42:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,6 @@ console.log('Loaded!');
 var elem=document.getElementById("main-text");
 elem.innerHtml="new value";
 var i=document.getElementById("madi");
-madi.onclick=function(){
-    madi.style.marginleft="100px";
+img.onclick=function(){
+    img.style.marginleft="100px";
 };
