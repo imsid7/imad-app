da7204751711bcd0fecbaddb538703323e990e8f
imsid7 <30745244+imsid7@users.noreply.github.com>
2017-08-06 15:11:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,3 @@
 console.log('Loaded!');
 var elem=document.getElementById('main-text');
 elem.innerHtml="new value";
-var i=document.getElementById('madi');
-i.onclick=function(){
-    i.style.marginleft="100px";
-};
