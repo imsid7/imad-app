ded9822c57bf1d60e2269a02e2698f9bbab11680
imsid7 <30745244+imsid7@users.noreply.github.com>
2017-08-06 16:08:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/submit-name' , function(req,res){
+app.get('/submit-name', function(req,res){
     var name;
     names.push(name);
     res.send(JSON.stringify(names));
